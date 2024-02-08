c5240692fb8884420222c24d9783c55f5b115dd4
syrga365 <153297624+syrga365@users.noreply.github.com>
2024-02-08 15:43:24 +0600

    hw4

--- book/urls.py
+++ book/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from post.views import (main_page_view,
                         hello_view,
@@ -39,3 +41,5 @@ urlpatterns = [
     path('category/', category_view, name='category'),
     path('category/<int:category_id>/', category_details_view),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
