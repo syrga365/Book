d0e508b8c59ed7f35e3434745dbb3babed09adf9
syrga365 <153297624+syrga365@users.noreply.github.com>
2024-02-08 13:07:05 +0600

    hw1

---
+++ post/views.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def main_page_view(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
+
+
+def hello_view(request):
+    if request.method == 'GET':
+        return HttpResponse("Hello, It's my project")
+
+
+def current_date_view(request):
+    if request.method == 'GET':
+        current_datetime = datetime.now()
+        return HttpResponse(current_datetime)
+
+
+def goodbye_view(request):
+    if request.method == 'GET':
+        return HttpResponse("Good bye, user!!!")
+
